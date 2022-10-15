3f54d15e85d37e52a1fdc3f2af673af8375b50a7
sunoo-lee <wldbsl312@gmail.com>
2022-10-16 04:03:20 +0900

    filter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Home = () => {
   const [select, setSelect] = useState([]);
+
+  const selectedKey = useRef([]);
+
   const onClick = (event) => {
     const keyword_btn = event.target;
     keyword_btn.className === "off"
@@ -13,6 +16,27 @@ const Home = () => {
       setSelect([...select, selected]);
     } else if (keyword_btn.className === "off") {
       setSelect(select.filter((key) => key !== selected));
+    }
+  };
+
+  const activeFilter = (event) => {
+    const keyword_btn = event.target;
+    keyword_btn.className === "list"
+      ? (keyword_btn.className = "list active")
+      : (keyword_btn.className = "list");
+
+    const selected = event.target.innerText;
+    if (keyword_btn.className === "list active") {
+      setSelect([...select, selected]);
+    } else if (keyword_btn.className === "list") {
+      setSelect(select.filter((key) => key !== selected));
+    }
+
+    for (let i = 0; i < selectedKey.current.length; ++i) {
+      console.log(selectedKey.current[i].dataset.item);
+      if (keyword_btn.dataset.filter === selectedKey.current[i].dataset.item) {
+        selectedKey.current[i].classList.toggle("hide");
+      }
     }
   };
   return (
@@ -84,6 +108,105 @@ const Home = () => {
           ))}
         </div>
         <div className="nav_toggle">검색</div>
+      </div>
+      {/* <div className="space">
+        <div className="star song_1">
+          <div className="song_info">
+            <span className="title">사건의 지평선</span>
+            <div className="thumbnail"></div>
+            <button className="song_play_btn">▶</button>
+          </div>
+        </div>
+        <div className="star song_2">Star2</div>
+        <div className="star song_3">Star3</div>
+      </div> */}
+      <div className="star_filter">
+        <ul>
+          <li onClick={activeFilter} className="list" data-filter="first">
+            #1집
+          </li>
+          <li onClick={activeFilter} className="list" data-filter="second">
+            #2집
+          </li>
+          <li onClick={activeFilter} className="list" data-filter="space">
+            #우주
+          </li>
+          <li onClick={activeFilter} className="list" data-filter="fall">
+            #가을
+          </li>
+          <li onClick={activeFilter} className="list" data-filter="rock">
+            #락
+          </li>
+        </ul>
+        <div className="product">
+          <div
+            ref={(item) => (selectedKey.current[0] = item)}
+            className="itemBox"
+            data-item="first"
+          >
+            <div className="item_img"></div>
+            <div className="song_info">
+              <span className="title">사건의 지평선</span>
+              <div className="thumbnail"></div>
+              <button className="song_play_btn">▶</button>
+            </div>
+          </div>
+          <div
+            ref={(item) => (selectedKey.current[1] = item)}
+            className="itemBox"
+            data-item="second"
+          >
+            <div className="item_img"></div>
+            <div className="song_info">
+              <span className="title">사건의 지평선</span>
+              <div className="thumbnail"></div>
+              <button className="song_play_btn">▶</button>
+            </div>
+          </div>
+          <div
+            ref={(item) => (selectedKey.current[2] = item)}
+            className="itemBox"
+            data-item="space"
+          >
+            <div className="item_img"></div>
+            <div className="song_info">
+              <span className="title">사건의 지평선</span>
+              <div className="thumbnail"></div>
+              <button className="song_play_btn">▶</button>
+            </div>
+          </div>
+          <div
+            ref={(item) => (selectedKey.current[3] = item)}
+            className="itemBox"
+            data-item="fall"
+          >
+            <div className="item_img"></div>
+            <div className="song_info">
+              <span className="title">사건의 지평선</span>
+              <div className="thumbnail"></div>
+              <button className="song_play_btn">▶</button>
+            </div>
+          </div>
+          <div
+            ref={(item) => (selectedKey.current[4] = item)}
+            className="itemBox"
+            data-item="rock"
+          >
+            <div className="item_img"></div>
+          </div>
+          <div
+            ref={(item) => (selectedKey.current[5] = item)}
+            className="itemBox"
+            data-item="space"
+          >
+            <div className="item_img"></div>
+            <div className="song_info">
+              <span className="title">사건의 지평선</span>
+              <div className="thumbnail"></div>
+              <button className="song_play_btn">▶</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/routes/Intro.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>
+        INTRO <br /> 데뷔 16주년 기념 화면
+      </h1>
+      ;
+    </div>
+  );
+};
+
+export default Intro;
