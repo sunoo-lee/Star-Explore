88addf4c5042783ab4e97a91c389961e38637aec
sunoo-lee <wldbsl312@gmail.com>
2022-10-18 04:23:56 +0900

    keyword filter prototype

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Home = () => {
   const [select, setSelect] = useState([]);
+  const [selectValue, setSelectValue] = useState([]);
 
   const selectedKey = useRef([]);
 
@@ -19,24 +20,35 @@ const Home = () => {
     }
   };
 
+  useEffect(() => {
+    // console.log("select: ", selectValue);
+    for (let i = 0; i < selectedKey.current.length; ++i) {
+      const keyValues = Object.values(selectedKey.current[i].dataset);
+      const intersection = selectValue.filter((item) =>
+        keyValues.includes(item)
+      );
+      if (selectValue.length !== intersection.length) {
+        selectedKey.current[i].classList.add("hide");
+      } else if (selectValue.length === intersection.length) {
+        selectedKey.current[i].classList.remove("hide");
+      }
+    }
+  }, [selectValue]);
+
   const activeFilter = (event) => {
     const keyword_btn = event.target;
     keyword_btn.className === "list"
       ? (keyword_btn.className = "list active")
       : (keyword_btn.className = "list");
 
-    const selected = event.target.innerText;
+    const selected = keyword_btn.innerText;
+    const selectedValue = keyword_btn.dataset.filter;
     if (keyword_btn.className === "list active") {
       setSelect([...select, selected]);
+      setSelectValue([...selectValue, selectedValue]);
     } else if (keyword_btn.className === "list") {
       setSelect(select.filter((key) => key !== selected));
-    }
-
-    for (let i = 0; i < selectedKey.current.length; ++i) {
-      console.log(selectedKey.current[i].dataset.item);
-      if (keyword_btn.dataset.filter === selectedKey.current[i].dataset.item) {
-        selectedKey.current[i].classList.toggle("hide");
-      }
+      setSelectValue(selectValue.filter((key) => key !== selectedValue));
     }
   };
   return (
@@ -109,17 +121,6 @@ const Home = () => {
         </div>
         <div className="nav_toggle">검색</div>
       </div>
-      {/* <div className="space">
-        <div className="star song_1">
-          <div className="song_info">
-            <span className="title">사건의 지평선</span>
-            <div className="thumbnail"></div>
-            <button className="song_play_btn">▶</button>
-          </div>
-        </div>
-        <div className="star song_2">Star2</div>
-        <div className="star song_3">Star3</div>
-      </div> */}
       <div className="star_filter">
         <ul>
           <li onClick={activeFilter} className="list" data-filter="first">
@@ -142,7 +143,8 @@ const Home = () => {
           <div
             ref={(item) => (selectedKey.current[0] = item)}
             className="itemBox"
-            data-item="first"
+            data-key1="first"
+            data-key2="fall"
           >
             <div className="item_img"></div>
             <div className="song_info">
@@ -154,7 +156,8 @@ const Home = () => {
           <div
             ref={(item) => (selectedKey.current[1] = item)}
             className="itemBox"
-            data-item="second"
+            data-key1="second"
+            data-key2="rock"
           >
             <div className="item_img"></div>
             <div className="song_info">
@@ -166,7 +169,8 @@ const Home = () => {
           <div
             ref={(item) => (selectedKey.current[2] = item)}
             className="itemBox"
-            data-item="space"
+            data-key1="first"
+            data-key2="space"
           >
             <div className="item_img"></div>
             <div className="song_info">
@@ -178,7 +182,9 @@ const Home = () => {
           <div
             ref={(item) => (selectedKey.current[3] = item)}
             className="itemBox"
-            data-item="fall"
+            data-key1="second"
+            data-key2="fall"
+            data-key3="space"
           >
             <div className="item_img"></div>
             <div className="song_info">
@@ -190,14 +196,18 @@ const Home = () => {
           <div
             ref={(item) => (selectedKey.current[4] = item)}
             className="itemBox"
-            data-item="rock"
+            data-key1="second"
+            data-key2="rock"
+            data-key3="fall"
           >
             <div className="item_img"></div>
           </div>
           <div
             ref={(item) => (selectedKey.current[5] = item)}
             className="itemBox"
-            data-item="space"
+            data-key1="first"
+            data-key2="rock"
+            data-key3="space"
           >
             <div className="item_img"></div>
             <div className="song_info">
