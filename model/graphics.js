94b70035a4b360db0a3445c71c07e9246f0ea593
JeeIn-Shin <wldls0530@gmail.com>
2022-11-19 03:03:58 +0900

    change release code location, subsitute error code

--- model/graphics.js
+++ model/graphics.js
@@ -2,10 +2,10 @@ const db = require("../config/database.js");
 //graphics의 정보를 가지고 화면이 구성이 됨
 
 const Graphics = function (songGraphics) {
-	this.songtitle = songGraphics.songtitle;
+	this.song_title = songGraphics.song_title;
 	this.coordinate = songGraphics.coordinate;
-	this.starDesign = songGraphics.starDesign;
-	this.recommend = songGraphics.recommend;
+	this.size = songGraphics.size;
+	this.image = songGraphics.image;
 }
 
 //곡 그래픽 정보를 모두 가져옴
@@ -14,24 +14,28 @@ Graphics.getAll = (result) => {
 	db.getConnection(function(err, connection) {
 		
 		if(!err) {
-			let query_to_find_all_graphic_info = "SELECT * FROM total_songs_graphic_information";
+			let query_to_find_all_graphic_info = `SELECT * FROM total_songs_graphic_information
+												  LIMIT 30`;
 
 			connection.query(query_to_find_all_graphic_info, (err, res) => {
-	
+				
+				connection.release();
+
 				if (err) {
 					console.log("error: ", err);
 					result(null, err);
 					return;
 				}
-		
-				console.log("songs: ", res);
+
+				console.log("found information: ", res);
 				result(null, res);
+				return;
 			})
-			connection.release();
 		}
-		connection.on('error', function(err)	{
+		else {
 			console.error('mysql connection error ' + err);
-		})
+			throw err;
+		}
 	})
 }
 
--- router/songGraphics.js
+++ router/songGraphics.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Graphics = require('../model/graphics');
 
 
-// http://localhost:8080/songGraphicsInformationRouter
+// http://localhost:8080/graphics
 router.get('/', (req, res) => {
     Graphics.getAll((err, data) => {
         if (err) {
