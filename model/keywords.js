7199992de5da0d5ad6e45e547ce4ea831439ee51
JeeIn-Shin <wldls0530@gmail.com>
2022-11-23 01:05:25 +0900

    search by keyword , only on

--- model/keywords.js
+++ model/keywords.js
@@ -1,8 +1,8 @@
 const db = require("../config/database.js");
 
-const Keyword = function(pos_keyword) {
-    this.keyword = pos_keyword.keyword;
-    this.column_name = pos_keyword.column_name;
+const Keyword = function(keyword_location) {
+    this.keyword = keyword_location.keyword;
+    this.column_name = keyword_location.column_name;
 }
 
 Keyword.getAll = (result) => {
@@ -34,48 +34,80 @@ Keyword.getAll = (result) => {
 	})
 }
 
+//선택된 키워드의 column 알아보기
+Keyword.find_keyword_index = (selected_keyword, result) => {
 
-
-
-// Information.find_keyword_index = (keyword, result) => {
-
-// 	db.getConnection(function (err, connection) {
+	db.getConnection(function (err, connection) {
 		
-// 		if (!err) {
-// 			console.log(SEsonginfo);
-// 			//첫문자부터, 해당 문자 포함한 노래 제목 | 발음 | 번안 검색
-// 			let query_to_select_keyword = `SELECT song_title, album_title, album_number, release_date, embedcode FROM total_songs_information
-// 									   WHERE song_title like '${SEsonginfo}%'
-// 									   OR pronunciation like '${SEsonginfo}%'
-// 									   OR translation like '${SEsonginfo}%'`;
+		if (!err) {
+			//console.log(selected_keyword);
 
-// 			connection.query(query_to_select_keyword, (err, res) => {
-				
-// 				connection.release();
-				
-// 				if (err) {
-// 					console.log("error: ", err);
-// 					result(err, null);
-// 					return;
-// 				}
+			let query_to_select_keyword = `SELECT attribute FROM keyword_list
+										   WHERE keyword like '${selected_keyword}'`;
 
-// 				//console.log(res.length);
-// 				if (res.length) {
-// 					console.log("found information: ", res);
-// 					result(null, res);
-// 					return;
-// 				}
-// 				//not found
-// 				console.log("not found");
-// 				result(null);
-// 			})
-// 		}
-// 		else {
-// 			console.error('mysql connection error ' + err);
-// 			throw err;
-// 		}
-// 	})
-// }
+			connection.query(query_to_select_keyword, (err, res) => {
+				
+				connection.release();
+				
+				if (err) {
+					console.log("error: ", err);
+					result(err, null);
+					return;
+				}
+
+				if (res.length) {
+					console.log("found information: ", res);
+					result(null, res);
+					return;
+				}
+				//not found
+				console.log("attribute not found");
+				result(null);
+			})
+		}
+		else {
+			console.error('mysql connection error ' + err);
+			throw err;
+		}
+	})
+}
+
+//특정 컬럼에서 해당 키워드로 검색하기
+Keyword.filter_out_songs_by_keyword = (attribute, selected_keyword, result) => {
+    
+    db.getConnection(function (err, connection) {
+		
+		if (!err) {
+
+			let query_to_filter_out_songs_by_keyword = `SELECT * FROM total_songs_keyword_information
+														WHERE ${attribute} like '${selected_keyword}'`;
+
+			connection.query(query_to_filter_out_songs_by_keyword, (err, res) => {
+				
+				connection.release();
+				
+				if (err) {
+					console.log("error: ", err);
+					result(err, null);
+					return;
+				}
+
+				if (res.length) {
+					console.log("found information: ", res);
+					result(null, res);
+					return;
+				}
+				//not found
+				console.log("song not found");
+				result(null);
+			})
+		}
+		else {
+			console.error('mysql connection error ' + err);
+			throw err;
+		}
+	}) 
+}
 
 
 
--- router/songKeywords.js
+++ router/songKeywords.js
@@ -23,4 +23,43 @@ router.get('/', (req, res) => {
     })
 })
 
+// http://localhost:8080/keywords/test
+router.get('/:selected_keyword', (req, res) => {
+    
+    Keyword.find_keyword_index(req.params.selected_keyword, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: 'attribute Not found song with search result.'
+                });
+            }
+            else {
+                res.status(500).send({
+                    message: 'attribute Error retrieving song with search result'
+                });
+            }
+        }
+        else {
+            let atb = data[0].attribute;
+            Keyword.filter_out_songs_by_keyword(atb, req.params.selected_keyword, (err, data) => {
+                if (err) {
+                    if (err.kind === "not_found") {
+                        res.status(404).send({
+                            message: 'song Not found song with keyword search result.'
+                        });
+                    }
+                    else {
+                        res.status(500).send({
+                            message: 'song Error retrieving song with keyword search result'
+                        });
+                    }
+                }
+                else
+                    res.json(data);
+            })
+        }
+
+    })
+})
+
 module.exports = router;
