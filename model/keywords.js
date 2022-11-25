ab757b9e9fcbeb9a3baae12e79cb59a114d82cae
JeeIn-Shin <wldls0530@gmail.com>
2022-11-25 21:23:33 +0900

    using req.query, implement multi-keyword-selected

--- model/keywords.js
+++ model/keywords.js
@@ -22,7 +22,7 @@ Keyword.getAll = (result) => {
 					return;
 				}
 
-				console.log("found information: ", res);
+				//console.log("found information: ", res);
 				result(null, res);
 				return;
 			})
@@ -35,18 +35,99 @@ Keyword.getAll = (result) => {
 }
 
 //선택된 키워드의 column 알아보기
-Keyword.find_keyword_index = (selected_keyword, result) => {
+// Keyword.find_keyword_index = (selected_keywords, result) => {
 
+// 	console.log(selected_keywords[0]);
+
+// 	db.getConnection(function (err, connection) {
+		
+// 		if (!err) {
+// 			//console.log(selected_keyword);
+
+// 			let query_to_select_keyword = `SELECT * FROM keyword_list`;
+
+// 			let sub_query_to_select_keyword = ` WHERE keyword like '${selected_keywords[0]}'`;
+
+
+// 			let completed_query = query_to_select_keyword + sub_query_to_select_keyword;
+
+// 			connection.query(completed_query, (err, res) => {
+				
+// 				connection.release();
+				
+// 				if (err) {
+// 					console.log("error: ", err);
+// 					result(err, null);
+// 					return;
+// 				}
+
+// 				if (res.length) {
+// 					//console.log("found information: ", res);
+// 					result(null, res);
+// 					return;
+// 				}
+// 				//not found
+// 				console.log("attribute not found");
+// 				result(null);
+// 			})
+// 		}
+// 		// else {
+// 		// 	console.error('mysql connection error ' + err);
+// 		// 	throw err;
+// 		// }
+// 	})
+// }
+
+//서브쿼리 만드는 함수
+let make_sub_query_to_select_keyword = (data) => {
+	
+	let sub_query_to_select_keyword = {};
+	let index;
+	let result;
+
+	//버튼이 하나만 선택됐을 때
+	if (Object.keys(data).length === 1)	{
+		
+		sub_query_to_select_keyword['_' + index] = ` WHERE ${Object.keys(data)[0]} like '${Object.values(data)[0]}'`;
+		return sub_query_to_select_keyword;
+	}
+	//버튼이 여러개 입력됐을 때
+	else	{
+		for(index = 0; index < Object.keys(data).length; index++)	{
+			//첫번째 버튼
+			if(index == 0)
+				sub_query_to_select_keyword['_' + index] = ` WHERE ${Object.keys(data)[index]} like '${Object.values(data)[index]}'`;
+			//그 외 버튼
+			else	{
+				sub_query_to_select_keyword['_' + index] = ` AND ${Object.keys(data)[index]} like '${Object.values(data)[index]}'`;
+			}
+
+		}
+		//console.log(sub_query_to_select_keyword);
+		result = Object.values(sub_query_to_select_keyword).join(' ') + ';';
+		//console.log("result :" + result);
+
+		return result
+	}
+
+
+}
+
+//선택된 키워드'들'로 노래를 검색해옴
+Keyword.filter_out_songs_by_keyword = (data, result) => {
+    
 	db.getConnection(function (err, connection) {
 		
 		if (!err) {
-			//console.log(selected_keyword);
 
-			let query_to_select_keyword = `SELECT attribute FROM keyword_list
-										   WHERE keyword like '${selected_keyword}'`;
+			let query_to_filter_out_songs_by_keyword = `SELECT * FROM total_songs_keyword_information`;
+			let whole_sub_query = make_sub_query_to_select_keyword(data);
 
-			connection.query(query_to_select_keyword, (err, res) => {
+			//let sub_query_to_select_keyword = ` WHERE ${Object.keys(data)[0]} like '${Object.values(data)[0]}'`;
+
+			connection.query(query_to_filter_out_songs_by_keyword + whole_sub_query, (err, res) => {
 				
+				//console.log(query_to_filter_out_songs_by_keyword + sub_query_to_select_keyword);
 				connection.release();
 				
 				if (err) {
@@ -56,44 +137,7 @@ Keyword.find_keyword_index = (selected_keyword, result) => {
 				}
 
 				if (res.length) {
-					console.log("found information: ", res);
-					result(null, res);
-					return;
-				}
-				//not found
-				console.log("attribute not found");
-				result(null);
-			})
-		}
-		else {
-			console.error('mysql connection error ' + err);
-			throw err;
-		}
-	})
-}
-
-//특정 컬럼에서 해당 키워드로 검색하기
-Keyword.filter_out_songs_by_keyword = (attribute, selected_keyword, result) => {
-    
-    db.getConnection(function (err, connection) {
-		
-		if (!err) {
-
-			let query_to_filter_out_songs_by_keyword = `SELECT * FROM total_songs_keyword_information
-														WHERE ${attribute} like '${selected_keyword}'`;
-
-			connection.query(query_to_filter_out_songs_by_keyword, (err, res) => {
-				
-				connection.release();
-				
-				if (err) {
-					console.log("error: ", err);
-					result(err, null);
-					return;
-				}
-
-				if (res.length) {
-					console.log("found information: ", res);
+					//console.log("found information: ", res);
 					result(null, res);
 					return;
 				}
@@ -109,6 +153,89 @@ Keyword.filter_out_songs_by_keyword = (attribute, selected_keyword, result) => {
 	}) 
 }
 
+// Keyword.find_keyword_index_multi = (selected_keyword1, selected_keyword2, result) => {
+	
+// 	db.getConnection(function (err, connection) {
+		
+// 		if (!err) {
 
+// 			let query_to_select_keyword = `SELECT * FROM keyword_list
+// 										   WHERE keyword like '${selected_keyword1}'
+// 										   OR keyword like '${selected_keyword2}'`;
+
+// 			connection.query(query_to_select_keyword, (err, res) => {
+				
+// 				connection.release();
+				
+// 				if (err) {
+// 					console.log("error: ", err);
+// 					result(err, null);
+// 					return;
+// 				}
+
+// 				if (res.length) {
+// 					console.log("found information: ", res);
+// 					result(null, res);
+// 					return;
+// 				}
+// 				//not found
+// 				console.log("attribute not found");
+// 				result(null);
+// 			})
+// 		}
+// 		else {
+// 			console.error('mysql connection error ' + err);
+// 			throw err;
+// 		}
+// 	})
+// }
+
+// Keyword.filter_out_songs_by_keyword_test = (attribute1, attribute2, selected_keyword1, selected_keyword2, result) => {
+    
+
+
+// 	db.getConnection(function (err, connection) {
+		
+// 		console.log('error!!!!!!!!');
+
+// 		// let attribute1 = keywords[0].attribute;
+// 		// let attribute2 = keywords[1].attribute;
+
+// 		// let selected_keyword1 = keywords[0].keyword;
+// 		// let selected_keyword2 = keywords[0].keyword;
+
+// 		if (!err) {
+
+// 			let query_to_filter_out_songs_by_keyword = `SELECT * FROM total_songs_keyword_information
+// 														WHERE ${attribute1} like '${selected_keyword1}'
+// 														AND ${attribute2} like '${selected_keyword2}'`;
+
+// 			connection.query(query_to_filter_out_songs_by_keyword, (err, res) => {
+				
+// 				//console.log(query_to_filter_out_songs_by_keyword + sub_query_to_select_keyword);
+// 				connection.release();
+				
+// 				if (err) {
+// 					console.log("error: ", err);
+// 					result(err, null);
+// 					return;
+// 				}
+
+// 				if (res.length) {
+// 					console.log("found information: ", res);
+// 					result(null, res);
+// 					return;
+// 				}
+// 				//not found
+// 				console.log("song not found");
+// 				result(null);
+// 			})
+// 		}
+// 		else {
+// 			console.error('mysql connection error ' + err);
+// 			throw err;
+// 		}
+// 	}) 
+// }
 
 module.exports = Keyword;
--- router/songKeywords.js
+++ router/songKeywords.js
@@ -23,9 +23,51 @@ router.get('/', (req, res) => {
     })
 })
 
+// router.get('/test/:temp1/:temp2', (req, res) => {
+
+//     // let selected_keywords = {};
+
+//     // for (let count = 0; count < 6; count++) {
+//     //     selected_keywords['keyword' + 1] = req.body.keyword;
+//     // }
+
+//     Keyword.find_keyword_index_multi(req.params.temp1, req.params.temp2, (err, data) => {
+//         if (err) {
+//             if (err.kind === "not_found") {
+//                 res.status(404).send({
+//                     message: 'attribute Not found song with search result.'
+//                 });
+//             }
+//             else {
+//                 res.status(500).send({
+//                     message: 'attribute Error retrieving song with search result'
+//                 });
+//             }
+//         }
+
+//         Keyword.filter_out_songs_by_keyword_test(data[0].attribute, data[1].attribute, data[0].keyword, data[1].keyword, (err, data) => {
+//             if (err) {
+//                 if (err.kind === "not_found") {
+//                     res.status(404).send({
+//                         message: 'song Not found song with keyword search result.'
+//                     });
+//                 }
+//                 else {
+//                     res.status(500).send({
+//                         message: 'song Error retrieving song with keyword search result'
+//                     });
+//                 }
+//             }
+//             else
+//                 res.json(data);
+//         })
+//     })
+// })
+
 // http://localhost:8080/keywords/test
-router.get('/:selected_keyword', (req, res) => {
-    
+
+router.get('/:selected_keyword1,', (req, res) => {
+
     Keyword.find_keyword_index(req.params.selected_keyword, (err, data) => {
         if (err) {
             if (err.kind === "not_found") {
@@ -60,6 +102,35 @@ router.get('/:selected_keyword', (req, res) => {
         }
 
     })
+})
+
+// http://localhost:8080/keywords/list
+// 사용방법
+// http://localhost:8080/keywords/list?{attribute}={keyword}
+// ex, http://localhost:8080/keywords/list?emotion1=시원한&emotion2=벅차는&genre=J-POP
+router.get('/list', (req, res) => {
+
+    //console.log(Object.keys(req.query));
+    //console.log(Object.values(req.query));
+   // console.log(Object.keys(req.query).length);
+
+    Keyword.filter_out_songs_by_keyword(req.query, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: 'song Not found song with keyword search result.'
+                });
+            }
+            else {
+                res.status(500).send({
+                    message: 'song Error retrieving song with keyword search result'
+                });
+            }
+        }
+        else
+            res.json(data);
+    })
+    //res.json(req.query);
 })
 
 module.exports = router;
