0feda90894e12e8a2bd3a4c01fd5eabcd927b518
sunoo-lee <wldbsl312@gmail.com>
2022-12-10 00:14:23 +0900

    intro modal done

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,19 +12,7 @@ const Intro = () => {
       <div className="intro_wrap">
         <div className="intro_container">
           <div onClick={onClick} className="intro_btn"></div>
-          <div className="inner_text">
-            <p>
-              <span>16</span>th
-            </p>
-            <span>anniversary</span>
-            <span>2008.12.17</span>
-            <span>YOUNHA Debut</span>
-            <hr />
-            <span>키워드와 검색으로</span>
-            <span>윤하의 노래를</span>
-            <span>탐험하세요.</span>
-          </div>
-          <div className="inner_img">{/* <Intro_img /> */}</div>
+          <div className="inner_img"></div>
         </div>
       </div>
     </div>
