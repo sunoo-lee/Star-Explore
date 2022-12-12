667ebc8a39dcb5638c94faaaaff5aa080c50a465
sunoo-lee <wldbsl312@gmail.com>
2022-12-12 20:10:58 +0900

    module delete

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../routes/Home";
 
 const AppRouter = () => {
+  const callApi = async () => {
+    axios.get("/api").then((res) => console.log(res.data));
+  };
+
+  useEffect(() => {
+    callApi();
+  }, []);
+
   return (
     <Router>
       <Routes>
